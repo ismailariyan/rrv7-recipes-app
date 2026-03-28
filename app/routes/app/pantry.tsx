a4e1bb556756422325c3a93344278af28008e8eb
ismailariyan <ariyan.git.8hxlz@aleeas.com>
2026-03-29 03:12:59 +0600

    add example usage for fetching data via prisma

--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -1,7 +1,21 @@
+import { useLoaderData } from "react-router";
+import type { PantryShelf } from "~/generated/prisma/client";
+import prisma from "~/lib/prisma";
+
+export async function loader() {
+  const shelves = await prisma.pantryShelf.findMany();
+  return { shelves };
+}
 export default function Pantry() {
+  const { shelves } = useLoaderData();
   return (
     <div>
       <p>This is the pantry page.</p>
+      <ul>
+        {shelves.map((shelf: PantryShelf) => (
+          <li key={shelf.id}>{shelf.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
