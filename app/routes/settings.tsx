da849f41b67cd038aa1dd50865e3c322f7f9324d
ismailariyan <ariyan.git.8hxlz@aleeas.com>
2026-03-28 17:18:00 +0600

    passing data using loader and also we can send obj from loader too

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,10 +1,20 @@
-import { Link, Outlet } from "react-router";
+import { Link, Outlet, useLoaderData } from "react-router";
+
+export function loader() {
+  return {
+    message: "Hello from the loader!",
+    date: new Date(Date.parse("2023-01-01")),
+  };
+}
 
 export default function Settings() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
       <h1>Settings</h1>
       <p> this is the settings page</p>
+      <p> Message from loader: {data.message}</p>
+      <p> Date from loader: {data.date.toLocaleDateString()}</p>
       <nav>
         <Link to="/settings/app">App</Link>
         <Link to="/settings/profile">Profile</Link>
